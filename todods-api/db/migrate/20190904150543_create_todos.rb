7187a8e59bb37aaec1e233ed36ffe2649b683b7b
Lichen Ma <lichenma123@gmail.com>
2019-09-04 12:35:18 -0400

    update

--- todods-api/db/migrate/20190904150543_create_todos.rb
+++ todods-api/db/migrate/20190904150543_create_todos.rb
@@ -1,9 +1,8 @@
 class CreateTodos < ActiveRecord::Migration[6.0]
   def change
     create_table :todos do |t|
-      t.string :name
-      t.boolean :done
-      t.references :todo, foreign_key: true
+      t.string :title
+      t.string :created_by
 
       t.timestamps
     end
